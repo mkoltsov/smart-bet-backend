a38f5f02cc8e6b8b070c3e20bd4c9607aee21ffa
mkoltsov <mikhail.koltsov@grandparade.co.uk>
2018-04-20 09:22:50 +0200

    totls added

--- index2.js
+++ index2.js
@@ -130,8 +130,9 @@ app.post(pfx + '/post', function (req, res) {
             console.log('NONCE');
             console.log(data);
             var non = data + 8;
+            console.log(non);
             var raw = {
-                "nonce": "0x0"+ non,
+                "nonce": non,
                 "gasPrice": "0x09502f9000",
                 "gasLimit": "0x8d41",
                 "to": "0x23964e7bda04c0e05fc448a00a3c8e21b2635416",
